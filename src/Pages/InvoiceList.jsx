893776f77a42a053e3ef7394b5951713d1b6461f
ijosue16 <iradukundajosue16@gmail.com>
2023-10-24 10:04:49 +0200

    added invoice modal

--- src/Pages/InvoiceList.jsx
+++ src/Pages/InvoiceList.jsx
@@ -1,12 +1,13 @@
-import React, {useEffect} from "react";
+import React, {useEffect,useState,useRef} from "react";
 import SuppliersListContainer from "../components/Listcomponents/SuppliersListContainer";
+import { useMyContext } from "../context files/LoginDatacontextProvider";
 import { Table } from "antd";
 import { useGetSuppliersInvoiceQuery, useGetAllInvoicesQuery } from "../states/apislice";
 import dayjs from "dayjs";
 import {PiDotsThreeVerticalBold} from "react-icons/pi";
 import { ImSpinner2 } from "react-icons/im";
 import {motion} from "framer-motion";
-import {BiSolidEditAlt} from "react-icons/bi";
+import {BiSolidDetail} from "react-icons/bi";
 import {MdOutlineClose, MdPayments} from "react-icons/md";
 import {FaSave} from "react-icons/fa";
 
@@ -14,17 +15,45 @@ const InvoiceList = () => {
     let dataz=[];
     // const {data, isLoading, isSuccess, isError, error} = useGetSuppliersInvoiceQuery();
     const { data, isLoading, isSuccess, isError, error } = useGetAllInvoicesQuery();
-    useEffect(() => {
-        
-    }, [isSuccess, isError, error]);
+    const { loginData } = useMyContext();
+    const{profile,permissions}=loginData;
+    const [showActions, SetShowActions] = useState(false);
+    const [selectedRow, SetSelectedRow] = useState(null);
+
     if (isSuccess) {
             const {invoices}=data.data;
             dataz=invoices;
             console.log(dataz);
         } else if (isError) {
             console.log(error.data.message);
-        }
+        };
+        let modalRef = useRef();
 
+        const handleClickOutside = (event) => {
+          if (!modalRef.current || !modalRef.current.contains(event.target)) {
+            SetShowActions(false);
+          }
+        };
+      
+        useEffect(() => {
+          document.addEventListener("click", handleClickOutside, true);
+          return () => {
+            document.removeEventListener("click", handleClickOutside, true);
+          };
+        }, []);
+          const handleActions = (id) => {
+            if (selectedRow === id) {
+              console.log("uri muduki sha");
+              SetShowActions(false);
+              SetSelectedRow("");
+            } else {
+              SetSelectedRow(id);
+              SetShowActions(true);
+              console.log("Clicked ID:", id);
+            }
+          };
+
+          
     const columns = [
         {
             title: "invoice #", 
@@ -125,74 +154,62 @@ const InvoiceList = () => {
         //         );
         //     },
         // },
-        // {
-        //     title: "Action",
-        //     dataIndex: "action",
-        //     key: "action",
-        //     render: (_, record) => {
-        //         const editable = isEditing(record);
-        //         return (
-        //             <>
-        //                 <div className="flex items-center gap-2">
-        //                     {editable ? null : (
-        //                         <>
-        //           <span className="relative">
-        //             <PiDotsThreeVerticalBold
-        //                 className=" text-xl"
-        //                 onClick={() => handleActions(record._id)}
-        //             />
-        //               {selectedRow === record._id && (
-        //                   <motion.ul
-        //                       ref={modalRef}
-        //                       animate={
-        //                           show
-        //                               ? {opacity: 1, x: -10, display: "block"}
-        //                               : {opacity: 0, x: 0, display: "none"}
-        //                       }
-        //                       className={` border bg-white z-20 shadow-md rounded absolute -left-[200px] w-[200px] space-y-2`}
-        //                   >
-        //                       <li
-        //                           className="flex gap-4 p-2 items-center hover:bg-slate-100"
-        //                           onClick={() => edit(record)}
-        //                       >
-        //                           <BiSolidEditAlt className=" text-lg"/>
-        //                           <p>edit</p>
-        //                       </li>
-        //                       {permissions.payments.create ? (
-        //                           <li
-        //                               className="flex gap-4 p-2 items-center hover:bg-slate-100"
-        //                               onClick={() => {
-        //                                   setSelectedLotNumber(record.lotNumber);
-        //                                   setShowPayModel(true);
-        //                               }}
-        //                           >
-        //                               <MdPayments className=" text-lg"/>
-        //                               <p>Pay</p>
-        //                           </li>
-        //                       ) : null}
-        //                   </motion.ul>
-        //               )}
-        //           </span>
-        //                         </>
-        //                     )}
+        {
+            title: "Action",
+            dataIndex: "action",
+            key: "action",
+            render: (_, record) => {
+                
+                return (
+                    <>
+                        <div className="flex items-center gap-2">
+                                <>
+                  <span className="relative">
+                    <PiDotsThreeVerticalBold
+                        className=" text-xl"
+                        onClick={() => handleActions(record._id)}
+                    />
+                      {selectedRow === record._id && (
+                          <motion.ul
+                              ref={modalRef}
+                              animate={
+                                  showActions
+                                      ? {opacity: 1, x: -10, display: "block"}
+                                      : {opacity: 0, x: 0, display: "none"}
+                              }
+                              className={` border bg-white z-20 shadow-md rounded absolute -left-[200px] w-[200px] space-y-2`}
+                          >
+                              <li
+                                  className="flex gap-4 p-2 items-center hover:bg-slate-100"
+                                  onClick={() => edit(record)}
+                              >
+                                  <BiSolidDetail className=" text-lg"/>
+                                  <p>details</p>
+                              </li>
+                              {/* {permissions.payments.create ? (
+                                  <li
+                                      className="flex gap-4 p-2 items-center hover:bg-slate-100"
+                                      onClick={() => {
+                                          setSelectedLotNumber(record.lotNumber);
+                                          setShowPayModel(true);
+                                      }}
+                                  >
+                                      <MdPayments className=" text-lg"/>
+                                      <p>Pay</p>
+                                  </li>
+                              ) : null} */}
+                          </motion.ul>
+                      )}
+                  </span>
+                                </>
+                            
 
-        //                     {editable ? (
-        //                         <div className="flex items-center gap-3">
-        //                             <FaSave
-        //                                 className=" text-xl"
-        //                                 onClick={() => save(record._id)}
-        //                             />
-        //                             <MdOutlineClose
-        //                                 className=" text-xl"
-        //                                 onClick={() => setEditRowKey("")}
-        //                             />
-        //                         </div>
-        //                     ) : null}
-        //                 </div>
-        //             </>
-        //         );
-        //     },
-        // },
+            
+                        </div>
+                    </>
+                );
+            },
+        },
     ];
 
 
