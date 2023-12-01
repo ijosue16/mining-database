0edc04e32a127c910c54bd92c3d2092599977c87
edenlisk <edenlisk>
2023-12-01 17:01:22 +0200

    Fix add new user to the chat

--- src/Pages/Chat/NewUserChat.jsx
+++ src/Pages/Chat/NewUserChat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect, useRef} from "react";
 import { FiEdit } from "react-icons/fi";
 import { IoFilterOutline } from "react-icons/io5";
 import { Avatar, message } from "antd";
@@ -8,21 +8,42 @@ import { current } from "@reduxjs/toolkit";
 import { positions } from "slate";
 import { Skeleton } from "antd";
 import { useSelector } from "react-redux";
+import {toInitialCase} from "../../components/helperFunctions";
 
-const NewUSerChart = ({ currentUser, visible, position }) => {
+const NewUSerChart = ({ chats, currentUser, setChats, visible, position, setNewUserModal }) => {
   const {userData}=useSelector(state => state.persistedReducer.global);
   const { data, isLoading, isSuccess, isError, error } = useGetAllUsersQuery();
   const [createNewChart,{isLoading:isCreating,isSuccess:isDone,isError:isFail,error:fail}]=useCreateChatMutation();
   // senderId,receiverId
   const [filteredUsers, setFilteredUsers] = useState([]);
   const [searchInput, setSearchInput] = useState("");
+  const [existingChats, setExistingChats] = useState([]);
+
+  let newChatRef = useRef();
+
+  const handleClickOutside = (event) => {
+    if (!newChatRef.current || !newChatRef.current.contains(event.target)) {
+      setNewUserModal(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (chats) {
+      setExistingChats(chats.map(chat => chat.members.find(member => member !== currentUser)));
+    }
+  }, [chats]);
 
   useEffect(() => {
     if (isSuccess) {
       const { users } = data.data;
-      const filterUsers = users.filter((user) =>
-        user.name.toLowerCase().includes(searchInput.toLowerCase())
-      );
+      const filterUsers = users.filter((user) => user.name.toLowerCase().includes(searchInput.toLowerCase()) && user._id !== userData._id && !existingChats.includes(user._id));
       setFilteredUsers(filterUsers);
     }
   }, [isSuccess, data, searchInput]);
@@ -30,11 +51,15 @@ const NewUSerChart = ({ currentUser, visible, position }) => {
   const handleSearchInputChange = (e) => {
     setSearchInput(e.target.value);
   };
-  const handleNewChartUser =async(id)=>{
+  const handleNewChartUser = async (id) => {
     const body={senderId:userData._id,receiverId:id};
-    await createNewChart({body});
-    console.log(body);
-
+    const response = await createNewChart({body});
+    if (response) {
+        const { newChat } = response.data.data;
+        setChats(prevState => ([...prevState, newChat]));
+        setNewUserModal(!visible);
+    }
+    setNewUserModal(!visible);
   };
 
 
@@ -44,6 +69,7 @@ const NewUSerChart = ({ currentUser, visible, position }) => {
         className={`bg-red-500 rounded-md shadow-lg h-96 max-w-72 absolute ${position} z-50 p-2 space-y-1 ${
           visible ? "block" : "hidden"
         }`}
+        ref={newChatRef}
       >
         <p className=" text-lg font-bold">New Chat</p>
         <input
@@ -69,14 +95,14 @@ const NewUSerChart = ({ currentUser, visible, position }) => {
                     <Skeleton.Input active />
                   </>
                 ): (
-                  <div onClick={()=>{handleNewChartUser(_id)}}>
+                  <div className="flex gap-2" onClick={()=>{handleNewChartUser(_id)}}>
                     <Avatar size={40} icon={<UserOutlined />} />
                     <div
                       className="name flex flex-col"
                       style={{ fontSize: "0.9rem" }}
                     >
                       <span className=" font-bold">{name}</span>
-                      <span className="">{role}</span>
+                      <span className="">{toInitialCase(role)}</span>
                     </div>
                   </div>
                 ) }
