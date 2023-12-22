56410845821d6f313322ea8a88d7ca09e68007b1
ijosue16 <iradukundajosue16@gmail.com>
2023-12-22 16:26:10 +0200

    added custom color

--- src/Layout/sidebar.jsx
+++ src/Layout/sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, Fragment } from "react";
+import { Button, Tooltip, ConfigProvider } from 'antd';
 import { useLocation, useNavigate } from "react-router-dom";
 import { PiCaretRightLight,PiSignInDuotone} from "react-icons/pi";
 
@@ -30,7 +31,7 @@ const Sidebar = ({ filteredMenu, opennav, open,logOut }) => {
                     </div>
                 </div>
             </div>
-            <div onClick={opennav} className={`-right-0 transition transform ease-in-out duration-500 flex border-4 border-white dark:border-[#0F172A] dark:hover:bg-blue-500 hover:bg-amber-500 absolute top-2 p-1 rounded-full text-white hover:rotate-45 ${!open && 'text-black'}`}>
+            <div onClick={opennav} className={`-right-0 transition transform ease-in-out duration-500 flex border-4 border-white dark:border-[#0F172A] dark:hover:bg-blue-500 hover:bg-custom_blue-500 absolute top-2 p-1 rounded-full text-white hover:rotate-45 ${!open && 'text-black'}`}>
                 {/* <PiWindowsLogoDuotone className={`${!open && 'text-black'}`} /> */}
                 <img src="https://icon-library.com/images/mining-icon/mining-icon-1.jpg" alt="logo-image" className="w-8 h-8" />
             </div>
@@ -45,7 +46,7 @@ const Sidebar = ({ filteredMenu, opennav, open,logOut }) => {
                                     // const navtext = title.toLowerCase().replace(/\s/g, '');
                                     return (
                                         <Fragment key={index}>
-                                            {!submenu && (<li key={id} className={`no-select flex flex-row items-center p-2 pl-6 space-x-3  hover:text-amber-500 hover:bg-amber-50 h-full w-full text-black transform ease-in-out duration-300  ${line && ' border-b mb-2'} ${active === navtext && ' bg-amber-100 text-amber-500 rounded'}`}
+                                            {!submenu && (<li key={id} className={`no-select flex flex-row items-center p-2 pl-6 space-x-3  hover:text-shark-900 hover:shadow-lg hover:bg-custom_blue-50 h-full w-full text-black transform ease-in-out duration-300  ${line && ' border-b mb-2'} ${active === navtext && ' bg-custom_blue-200 text-custom_blue-700 rounded'}`}
                                                 onClick={() => { navigate(`/${navtext}`); setActive(navtext) }}
                                             >
                                                 <span className="w-4 h-4 ">{icon}</span>
@@ -62,7 +63,7 @@ const Sidebar = ({ filteredMenu, opennav, open,logOut }) => {
                                                     onClick={() => { setActive(navtext) }}
                                                 // ${active === navtext && ' bg-amber-100 text-amber-500 rounded'}
                                                 >
-                                                    <div className="flex flex-row items-center p-2 pl-6 gap-3 justify-start hover:text-amber-500 hover:bg-amber-50 h-full w-full" onClick={() => {
+                                                    <div className=" no-select flex flex-row items-center p-2 pl-6 gap-3 justify-start hover:text-shark-900 hover:shadow-lg hover:bg-custom_blue-50 h-full w-full" onClick={() => {
                                                         setOpenSubmenu(!openSubmenu);
                                                         setChoseNav(id)
                                                     }}>
@@ -71,11 +72,11 @@ const Sidebar = ({ filteredMenu, opennav, open,logOut }) => {
                                                         {submenu && <PiCaretRightLight className={`text-xs duration-500 ${openSubmenu && id === choseNav && 'rotate-90'}`} />}
                                                     </div>
                                                     {submenu && id === choseNav && (
-                                                        <ul className={` pl-12 space-y-4 list-disc pb-2  ${openSubmenu ? 'block' : 'hidden'}`}>
+                                                        <ul className={` pl-12 space-y-4 list-disc pb-2 ${openSubmenu ? 'block' : 'hidden'}`}>
                                                             {submenuItems.map(({ id, title,navtext }) => {
                                                                 // const navtext = title.toLowerCase().replace(/\s/g, '')
                                                                 return (
-                                                                    <li key={id} className="hover:text-amber-500"
+                                                                    <li key={id} className="hover:text-custom_blue-500"
                                                                         onClick={() => { navigate(`/${navtext}`) }}
                                                                     >
                                                                         {title}
@@ -111,7 +112,7 @@ const Sidebar = ({ filteredMenu, opennav, open,logOut }) => {
                 </ul>
             </div>
             {/* small sidebar menu */}
-            <ul className={`mini mt-20 flex flex-col space-y-2 w-full list-none ${open && 'hidden'}`}
+            <ul className={`mini mt-20 overflow-y-scroll overflow-x-hidden grid space-y-2 w-full list-none ${open && 'hidden'}`}
             //  onMouseEnter={opennav}
             >
                 {filteredMenu.map(({ subHeaders }, index) => {
@@ -121,18 +122,25 @@ const Sidebar = ({ filteredMenu, opennav, open,logOut }) => {
                                 const navtext = title.toLowerCase().replace(/\s/g, '');
                                 return (
                                     <Fragment key={index}>
-                                        <li key={index} className={`hover:ml-4 justify-end pr-5 text-black hover:text-amber-500 hover:bg-amber-50 w-full p-3 rounded transform ease-in-out duration-300 flex
-                ${active === navtext && ' bg-amber-100 text-amber-800 rounded'}
+                                        <Tooltip title={title} placement="rightTop" color={"#fff"} overlayInnerStyle={{color:"black"}} trigger={["hover"]}>
+                                        <li key={index} className={`hover:ml-4 justify-end pr-5 text-black hover:text-custom_blue-500 hover:bg-custom_blue-100 hover:shadow-md w-full p-3 rounded transform ease-in-out duration-300 flex
+                ${active === navtext && ' bg-custom_blue-200 text-custom_blue-700 rounded'}
                 `} 
                                         >
                                             <span className="w-4 h-4 " onClick={opennav}>{icon}</span>
                                         </li>
+                                        </Tooltip>
                                     </Fragment>
                                 )
                             })}
                         </Fragment>)
                 })}
-
+        <li  className={`hover:ml-4 justify-end pr-5 hover:text-custom_blue-500 hover:bg-custom_blue-100 hover:shadow-md w-full p-3 rounded transform ease-in-out duration-300 flex
+               
+                `} 
+                                        >
+                                            <span className="w-4 h-4 "><PiSignInDuotone /></span>
+                                        </li>
             </ul>
         </aside>
 
